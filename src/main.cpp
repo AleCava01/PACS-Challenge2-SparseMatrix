cf86df17714d1a47ec88d7afb754d6254769ddae
Mattia Del Grande <mattiadelgrande@Mattias-MacBook-Pro.local>
2025-04-12 17:06:31 +0200

    merge

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,19 @@
 using namespace algebra;
 
 int main(){
-    Matrix<int, algebra::StorageOrder::RowMajor> mat(5, 8);
+    Matrix<int, algebra::StorageOrder::RowMajor> mat(1000, 1000);
+
     
+    //for (int r : 1000) for (auto& x : r) if ((double)rand()/RAND_MAX > 0.95) x = rand() % 100 + 1;
+    
+    for (int i = 0; i < 1000; i++){
+        for (int j = 0; j < 1000; j++){
+            if ((double)rand()/RAND_MAX > 0.999){
+                mat.update(i,j,1);
+            }
+        }
+    }
+
     mat.print();
     mat.update(1,2,4);
     
